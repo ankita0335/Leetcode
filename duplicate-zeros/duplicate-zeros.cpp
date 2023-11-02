4d96781dc8443cae00121947ed67bde1aa93dff1
Ankita Chaturvedi <119069509+ankita0335@users.noreply.github.com>
2023-11-02 22:40:28 +0530

    Time: 77 ms, Memory: 9.9 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        vector<int>v;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                for(int j=arr.size()-2;j>=i;j--){
+                    arr[j+1]=arr[j];
+                }
+                i++;
+            }
+        }
+    }
+};
